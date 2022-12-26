dc0cd02fcbfce4148f30605c154e7740c4c9e81e
tambatramdani3 <tambatramdani2304@gmail.com>
2022-12-26 22:13:28 +0700

    tambah Controler Peta dan Edit Home

---
+++ app/Controllers/PetaController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controllers;
+
+class PetaController extends BaseController
+{
+    public function index()
+    {
+        helper('form');
+        $model = new \App\Models\DataModel();
+
+        // $fileName = base_url("maps/prov.geojson");
+        $file = file_get_contents("./maps/bandarLampung.geojson");
+        $file = json_decode($file);
+
+        $features = $file->features;
+
+        $idMasterData = 1;
+		if($this->request->getPost())
+		{
+			$idMasterData = $this->request->getPost('master');
+		}
+
+        foreach ($features as $index => $feature) {
+            $kode_wilayah = $feature->properties->kode;
+            $data = $model->where('id_master_data', $idMasterData)
+                ->where('kode_wilayah', $kode_wilayah)
+                ->first();
+
+            if ($data) {
+                $features[$index]->properties->nilai = $data->nilai;
+            }
+        }
+
+        $nilaiMax = $model->select('MAX(nilai) AS nilai')->where('id_master_data', $idMasterData)->first()->nilai;
+
+        $masterDataModel = new \App\Models\MasterDataModel();
+		$masterData = $masterDataModel->find($idMasterData);
+
+        $allMasterData = $masterDataModel->findAll();
+
+        $masterDataMenu = [];
+
+        foreach($allMasterData as $md)
+		{
+			$masterDataMenu[$md->id] = $md->nama;
+		}
+
+        return view(
+            'pages/persebaran_penduduk',
+            [
+                'data' => $features,
+                'nilaiMax' => $nilaiMax,
+                'masterData' => $masterData,
+                'masterDataMenu' => $masterDataMenu,
+            ]
+        );
+    }
+
+    public function halaman_penduduk_miskin()
+    {
+        return view('pages/penduduk_miskin');
+    }
+}
