7e8036767352d3e85d2115b73ff503f60976c590
assyfanaziwaa <assyfanaziwa8@gmail.com>
2022-12-26 22:32:33 +0700

    tambah model

---
+++ app/Models/KodeWilayahModel.php
@@ -0,0 +1,12 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class KodeWilayahModel extends Model{
+    protected $table = 'kode_wilayah';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['id', 'kode_wilayah', 'nama_wilayah'];
+    protected $returnType = '\App\Entities\Kodewilayah';
+    protected $useTimestamps = false;
+
+}
